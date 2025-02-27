c86b3a1c91a5ce929efd943d2ac005f3c89fe2ab
Melvin Escobar <melvin.escobar@macaulay.cuny.edu>
2025-02-27 03:51:18 -0500

    Add Movie Functionality

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 import Model from './model.js';
 
 window.onload = function() {
+  // Get elements
   const model = new Model();
+  const popup = document.getElementById("popup-window");
+  const openPopupBtn = document.getElementById("open-popup");
+  const closePopupBtn = document.getElementById("close-popup");
+  const submitBtn = document.getElementById("submit-popup");
 
+  displayMovies(model.data.movies);
+
+  //List all Movies by default
   function displayMovies(movieList) {
     const container = document.getElementById('movies-container');
     container.innerHTML = `
@@ -21,9 +29,9 @@ window.onload = function() {
             <tbody id="movie-table-body"></tbody>
         </table>
     `;
-
     const tableBody = document.getElementById('movie-table-body');
 
+    
     movieList.forEach(movie => {
         const row = `
             <tr>
@@ -37,16 +45,48 @@ window.onload = function() {
         `;
         tableBody.innerHTML += row;
     });
-}
-
-  displayMovies(model.data.movies);
-
-  const openPopup =  Boolean(false);
-  function popup() {
-    document.getElementById('popup').style.display = 'block';
   }
 
-  function closePopup() {
-    document.getElementById('popup').style.display = 'none';
+  function addMovie(){
+    const title = document.getElementById('movie-title').value.trim();
+    const genre = document.getElementById('movie-genre').value.trim();
+    const movieBy = document.getElementById('movie-by').value.trim();
+    const movieDate = parseInt(document.getElementById('movie-genre').value.trim());
+    const rating = parseInt(document.getElementById('movie-rating').value);
+    const status = document.getElementById('movie-status').value.trim();
+
+    if(!title || !genre || !movieBy || isNaN(rating ||rating < 0 || rating > 10)) {
+      alert('Please fill in all fields correctly.');
+      return;
+    }
+
+    const newMovie = {
+      movieID: 'm' + (model.data.movies.length + 1),
+      title,
+      genre,
+      movieBy,
+      movieDate,
+      rating,
+      status
+    };
+    
+    popup.style.display = "none";
+    model.data.movies.push(newMovie);
+    displayMovies(model.data.movies);    
   }
+
+  //CODE FOR BUTTONS
+
+  // Open Popup
+  openPopupBtn.addEventListener("click", () => {
+      popup.style.display = "flex";
+  });
+
+  // Close Popup
+  closePopupBtn.addEventListener("click", () => {
+      popup.style.display = "none";
+  });
+
+  // Submit and Close Popup
+  submitBtn.addEventListener("click", addMovie);
 };
